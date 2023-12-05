ccd846fe81464ad8d9783757f8c7defbd553cb18
Taiwo1105 <t.adegboyeg@alustudent.com>
2023-12-05 11:21:25 -0800

    Full creation

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,10 @@
--- Create_table 'second _table' in database 'hbtn_0c_0'
--- Add-props  (id INT), (name VAR-CHAR(256)), (score INT)
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-
--- Script should create maximum of 4 records
-INSERT INTO 'second_table' ('id', 'name', 'score') VALUES (1, "John", 10);
-INSERT INTO 'second_table' ('id', 'name', 'score') VALUES (2, "Alex", 3);
-INSERT INTO 'second_table' ('id', 'name', 'score') VALUES (3, "Bob", 14);
-INSERT INTO 'second_table' ('id', 'name', 'score') VALUES (4, "George", 8);
+-- SECOND TABLE
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+INSERT INTO second_table (id, name, score) VALUES(1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES(2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES(3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES(4, 'George', 8); 
